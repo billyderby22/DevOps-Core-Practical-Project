d9c99f28301deb44bacf8eeda25d97b41d821eef
billyderby <billyderby97@gmail.com>
2022-04-04 10:55:26 +0000

    added docker-compose

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -1,13 +1,8 @@
-from application import app, db
-from application.models import Results
-from flask import render_template
-import requests
+from application import app
+import requests 
 
 @app.route('/')
 def index():
     player = requests.get('http://player-api:5000/get-player')
     team = requests.post('http://team-api:5000/team', json=player.json())
-    db.session.add(Results(player=player.json()["player"], team=team.json()["team"]))
-    db.session.commit()
-    results = Results.query.all()
-    return render_template('index.html', results = results)
+    return f'{player.json()["player"]} plays for {team.json()["team"]}'
