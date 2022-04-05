f65007b11f78fe22f5d6a5f189dbf3fc7a54957b
billyderby <billyderby97@gmail.com>
2022-04-05 11:32:45 +0000

    written tests

---
+++ position-api/tests/test_position_api.py
@@ -0,0 +1,13 @@
+from application import app
+from flask_testing import TestCase
+from flask import url_for
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestView(TestBase):
+    def test_get_position_Noble(self):
+        response = self.client.post(url_for('position'), json={"player":"Noble"})
+        self.assert200(response)
+        self.assertIn(b'Midfielder', response.data)
